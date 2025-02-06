97b0b11183189d5a8a32b84052f2b4a9c52f94ba
Michał Bloch <112960926+Dnafivuq@users.noreply.github.com>
2025-02-06 15:48:29 +0100

    moved friction into velocity calculation

--- physical_body.py
+++ physical_body.py
@@ -8,10 +8,27 @@ mu = 0.9
 k = 0.01
 
 
-def round_to_zero(value: float, threshold: float = 0.005) -> float:
-    if value**2 < threshold:
-        return 0
-    return value
+def normalize_vector(vector: Vector2) -> Vector2:
+    lenght = np.sqrt(vector.x**2 + vector.y**2)
+    if lenght != 0:
+        return Vector2(
+            vector.x / lenght,
+            vector.y / lenght
+        )
+    return vector
+
+
+def round_to_zero(vector: Vector2, acceleration: Vector2, threshold: float = 0.005) -> Vector2:
+    x = vector.x
+    y = vector.y
+    if (x**2 < threshold and acceleration.x**2 < threshold):
+        x = 0
+    if (y**2 < threshold and acceleration.y**2 < threshold):
+        y = 0
+    return Vector2(
+        x,
+        y
+    )
 
 
 class PhysicalBody:
@@ -26,14 +43,16 @@ class PhysicalBody:
     def update(self, applied_force_acceleration: Vector2, delta_time: float) -> None:
 
         self._acceleration = Vector2(
-            applied_force_acceleration.x * self._speed - np.sign(self._velocity.x) * (g*mu + k*self._velocity.x**2),
-            applied_force_acceleration.y * self._speed - np.sign(self._velocity.y) * (g*mu + k*self._velocity.y**2)
+            applied_force_acceleration.x * self._speed - np.sign(self._velocity.x) * k*self._velocity.x**2,
+            applied_force_acceleration.y * self._speed - np.sign(self._velocity.y) * k*self._velocity.y**2
         )
 
         self._velocity = Vector2(
-            round_to_zero(self._velocity.x + self._acceleration.x * delta_time),
-            round_to_zero(self._velocity.y + self._acceleration.y * delta_time)
+            self._velocity.x + (self._acceleration.x - normalize_vector(self._velocity).x*mu*g) * delta_time,
+            self._velocity.y + (self._acceleration.y - normalize_vector(self._velocity).y*mu*g) * delta_time
         )
+        self._velocity = round_to_zero(self._velocity, self._acceleration)
+
         self._position = Vector2(
             self._position.x + self._velocity.x * delta_time,
             self._position.y + self._velocity.y * delta_time
